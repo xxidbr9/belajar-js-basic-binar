36af599f6671036b1163d5bc4c7a403ea46085aa
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2023-06-23 22:08:59 +0700

    feat: function

---
+++ 03_function/index.js
@@ -0,0 +1,20 @@
+// function declaration
+function sayHello() {
+  this.name = "nando";
+}
+
+// variable
+const sayMyName = () => {};
+const sayMyName2 = () => ""; // short hand syntax => atau langsung return
+
+// argument
+const helloWorld = (nama, umur) => `Hai saya ${nama}, umur saya ${umur}`;
+console.log(helloWorld("nando", 27));
+
+const siswa = new sayHello();
+console.log(siswa);
+
+const pertambahan = (a, b) => a + b;
+console.log(pertambahan(1, 2));
+console.log(pertambahan(2, 3));
+console.log(pertambahan(2, pertambahan(1, 2)));
