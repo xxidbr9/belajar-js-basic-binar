8d6874e79a0dae6a671292fdc8c6ee834c2014b8
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2023-08-23 21:38:16 +0700

    fix: add nodemon

--- 16_knex-relasi/configs/db.config.js
+++ 16_knex-relasi/configs/db.config.js
@@ -2,5 +2,7 @@ const knex = require("knex");
 const knexFile = require("../knexfile");
 
 const db = knex(knexFile["development"]);
-
+db.on("query", function (queryData) {
+  console.log(queryData);
+});
 module.exports = db;
