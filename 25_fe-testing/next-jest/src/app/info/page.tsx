c5041e7969384f31616ebadbd5f1b835f2b95e1a
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2023-10-09 20:32:38 +0700

    fix: add nodemon

--- 25_fe-testing/next-jest/src/app/info/page.tsx
+++ 25_fe-testing/next-jest/src/app/info/page.tsx
@@ -8,12 +8,15 @@ type Props = {}
 
 const page = (props: Props) => {
   const { value, setValue } = useStore()
+  const isAlreadyLogin = false;
   return (
     <div>
       <h1>Hello</h1>
       <div>{value}</div>
       <div>
         <Button onClick={setValue}>Add 1</Button>
+        {isAlreadyLogin && <div>Sign Out</div>}
+        {!isAlreadyLogin && <div>Login</div>}
       </div>
     </div>
   )
