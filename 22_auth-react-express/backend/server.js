905a7878c6cdf7e39dfb7211b4893a30b14646b7
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2023-10-11 20:42:47 +0700

    feat: add concurrently runner

--- 22_auth-react-express/backend/server.js
+++ 22_auth-react-express/backend/server.js
@@ -15,6 +15,8 @@ const JWT_KEY = "binar-jwt-key";
 
 app.use(bodyParser({ extended: true }));
 app.use(cors({ origin: "*" }));
+
+// register
 app.post("/api/v1/user/register", async (req, res) => {
   /* 
     email
@@ -25,11 +27,15 @@ app.post("/api/v1/user/register", async (req, res) => {
   const id = v4();
   const genSalt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, genSalt);
+
+  // insert ke table user untuk menyimpan email dan password
   await db("users").insert({
     id,
     email,
     hash_password: hashPassword
   });
+
+  // insert ke profile untuk menyimpan informasi tambahan dari user
   await db("user_profiles").insert({ user_id: id, full_name: fullname });
 
   // success
@@ -52,11 +58,14 @@ app.post("/api/v1/user/login", async (req, res) => {
     .first("id", "email", "hash_password")
     .where("email", email);
 
+  if (!user) return res.status(404).json({ error: "email tidak terdaftar" });
+
   const isSameUser = await bcrypt.compare(password, user.hash_password);
   if (!isSameUser) {
-    return res.status(403).json({ error: "email or password wrong" });
+    return res.status(404).json({ error: "email or password wrong" });
   }
 
+  // mengisi data di dalam jwt/access token
   const userJwt = { user: { id: user.id, email: user.email } };
   const access_token = jwt.sign(userJwt, JWT_KEY, {
     issuer: "binar",
@@ -74,15 +83,19 @@ app.post("/api/v1/user/login", async (req, res) => {
 // middleware
 const authMiddleware = async (req, res, next) => {
   const token = req.headers.authorization;
+
+  // split: Bearer <token>
   const accessToken = token.split(" ")[1];
 
   const infoJwt = jwt.verify(accessToken, JWT_KEY);
+
+  // ini di tambah ke req
   req.local_user = infoJwt.user;
   next();
 };
 
-const loginController = async (req, res) => {
-  const userJwt = req.local_user;
+const profileController = async (req, res) => {
+  const userJwt = req.local_user; // access req dari middleware
 
   const user = await db("user_profiles")
     .first("full_name")
@@ -98,7 +111,7 @@ const loginController = async (req, res) => {
     }
   });
 };
-app.get("/api/v1/user/profile", authMiddleware, loginController);
+app.get("/api/v1/user/profile", authMiddleware, profileController);
 
 app.listen(9000, () => {
   console.log("http://localhost:9000");
--- 22_auth-react-express/frontend/src/App.jsx
+++ 22_auth-react-express/frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const App = () => {
-  // return <div>App</div>;
-  // return <Register />;
-  return <Login />;
-};
-
-const Register = () => {
+export const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [fullname, setFullname] = useState("");
   const [resp, setResp] = useState({});
+  const navigate = useNavigate();
+
   const handleRegister = async () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -30,37 +28,49 @@ const Register = () => {
 
     fetch("http://localhost:9000/api/v1/user/register", requestOptions)
       .then(response => response.json())
-      .then(result => setResp(result))
+      .then(result => {
+        setResp(result);
+        navigate("/sign-in");
+      })
       .catch(error => console.log("error", error));
   };
 
   return (
     <div>
-      <input
-        type='text'
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        type='text'
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <input
-        type='text'
-        value={fullname}
-        onChange={e => setFullname(e.target.value)}
-      />
-      <button onClick={handleRegister}>submit</button>
-      <pre>{JSON.stringify(resp, null, 2)}</pre>
+      <div>
+        <Link to={"/sign-in"}>Login</Link>
+      </div>
+      <div>
+        <input
+          type='text'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder='email'
+        />
+        <input
+          type='text'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder='password'
+        />
+        <input
+          type='text'
+          value={fullname}
+          onChange={e => setFullname(e.target.value)}
+          placeholder='fullname'
+        />
+        <button onClick={handleRegister}>submit</button>
+        <pre>{JSON.stringify(resp, null, 2)}</pre>
+      </div>
     </div>
   );
 };
 
-const Login = () => {
+export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [resp, setResp] = useState({});
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     var myHeaders = new Headers();
@@ -82,27 +92,102 @@ const Login = () => {
       .then(response => response.json())
       .then(result => {
         localStorage.setItem("accessToken", result.data.access_token);
-        setResp(result)
+        setResp(result);
+        navigate("/");
       })
       .catch(error => console.log("error", error));
   };
 
   return (
     <div>
-      <input
-        type='text'
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        type='text'
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>submit</button>
-      <pre>{JSON.stringify(resp, null, 2)}</pre>
+      <div>
+        <Link to={"/sign-up"}>Register</Link>
+      </div>
+      <div>
+        <input
+          type='text'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          type='text'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>submit</button>
+        <pre>{JSON.stringify(resp, null, 2)}</pre>
+      </div>
     </div>
   );
 };
 
-export default App;
+export const Profile = () => {
+  const [resp, setResp] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useAlreadyLogin();
+  const navigate = useNavigate();
+
+  const getUserProfile = async () => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      setIsLoading(false);
+      return;
+    } // berhenti jika tidak ada accessToken
+
+    const userProfile = await getUserProfileApi(accessToken);
+    setResp(userProfile);
+    setIsLoading(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    navigate("/sign-in");
+  };
+
+  useEffect(() => {
+    getUserProfile();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button onClick={handleLogout}>Logout</button>
+      {isLoading && <div>Loading....</div>}
+      {!isLoading && (
+        <div>
+          <pre>{JSON.stringify(resp, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+// hooks check is login
+const useAlreadyLogin = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) navigate("/sign-in");
+  }, []);
+};
+
+// function call profile
+const getUserProfileApi = async accessToken => {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", `Bearer ${accessToken}`); // tergantung di backend, Bearer hanya untuk standarisasi
+
+  const requestOptions = {
+    headers,
+    method: "GET"
+  };
+
+  const resp = await fetch(
+    "http://localhost:9000/api/v1/user/profile",
+    requestOptions
+  );
+  return await resp.json();
+};
